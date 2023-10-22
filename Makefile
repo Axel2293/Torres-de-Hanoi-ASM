af4addb9bba30b730076c5e983b4d8a4a912f964
Axel Escoto Garcia <axel.escoto@iteso.mx>
2023-10-22 16:00:48 -0600

    Algoritmo en C final

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 
-compilar: algoritmo.c 
+all: compile exec
+
+compile: algoritmo.c 
 	gcc algoritmo.c -o hanoi_c.exec
-	./hanoi_c.exec
-	
-execute: hanoi_c.exec
+
+exec: compile
 	./hanoi_c.exec
 
-limpiar: hanoi_c.exec
+clean: hanoi_c.exec
 	rm -f hanoi_c.exec
--- algoritmo.c
+++ algoritmo.c
@@ -5,12 +5,14 @@
 #define NUMDISK 3
 
 void print_towers (int32_t n, int32_t *tA, int32_t *tB, int32_t *tC);
+int8_t addtotower(int32_t n, int32_t disk, int32_t*tower);
+void hanoi(int32_t N, int32_t *origen, int32_t *auxiliar, int32_t *destino);
+int32_t pop (int32_t *st);
+
+int moves=0;
 
 int main(){
 
-    printf("Sizeof int 32: %d \n", sizeof(int32_t));
-
-    // FIFO Towers structures
     int32_t *torre_A = calloc(NUMDISK, sizeof(int32_t));
     printf("%p\n", torre_A);
     int32_t *torre_B = calloc(NUMDISK, sizeof(int32_t));
@@ -19,8 +21,8 @@ int main(){
     printf("%p\n", torre_C);
 
 
-    printf("Agregando circulos a torres:\n");
-    for(int32_t i=NUMDISK; i>0; i--){
+    printf("Agregando discos a torres:\n");
+    for(int32_t i=1; i<=NUMDISK; i++){
         *torre_A = i;
         printf("%p\n", torre_A);
         torre_A+=1;
@@ -29,11 +31,15 @@ int main(){
 
     print_towers(NUMDISK, torre_A, torre_B, torre_C);
 
+    hanoi(NUMDISK, torre_A, torre_B, torre_C);
+
+    printf("\nTOTAL MOVES: %d\n", moves);
+
 }
 
 void print_towers(int32_t n, int32_t *tA, int32_t *tB, int32_t *tC){
     
-    printf("%p : [", tA);
+    printf("\n TOWERS:\n[");
     for (size_t i = 0; i < n; i++)
     {
         printf("%d,", *tA);
@@ -63,23 +69,57 @@ void print_towers(int32_t n, int32_t *tA, int32_t *tB, int32_t *tC){
 void hanoi(int32_t N, int32_t *origen, int32_t *auxiliar, int32_t *destino){
     if (N == 1)
     {
-        int32_t disk = *origen;
-         
+        // get the disk at origin tower (Only one disk left)
+        int32_t disk = pop(origen);
+        // Move it to dest tower
+        addtotower(NUMDISK, disk, destino);
+        moves++;
+        printf("After moving disk %d to dest tower:\n", disk);
+        print_towers(NUMDISK, origen, auxiliar, destino);
+        return;
     }
-    
+    // Destiny now becomes auxiliar tower
+    hanoi(N-1, origen, destino, auxiliar);
+    int32_t disk = pop(origen);
+    printf("After moving disk %d to dest tower:\n", disk);
+    addtotower(NUMDISK, disk, destino);
+    moves++;
+    print_towers(NUMDISK, origen, auxiliar, destino);
+    // Auxiliar is now origin, and origin is now auxiliar
+    hanoi(N-1, auxiliar, origen, destino);
+
 }
 
 int8_t addtotower(int32_t n, int32_t disk, int32_t*tower){
-    // Move on the array then find the free spot to place the disk
+    // Move on the array then find the free spot to place the disk at the top of the stack
+    tower+=n-1;
     for (int32_t i = 0; i < n; i++)
     {
         if (*tower == 0)
         {
             *tower = disk;
+            return 1;
         }else{
-            tower++;
+            tower--;
         }
         
     }
+    return 0;
+    
+}
+
+int32_t pop (int32_t *st){
+    for (size_t i = 0; i < NUMDISK; i++)
+    {
+        if(*st!=0){
+            int32_t disk_pop = *st; 
+            *st=0;
+            return disk_pop;
+        }
+        else{
+            st++;
+        }
+    }
+    return 0;
     
 }
